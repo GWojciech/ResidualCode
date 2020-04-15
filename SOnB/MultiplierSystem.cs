b831198e5537174e9fbe67bf3001792a99397368
GWojciech <woitek@autograf.pl>
2020-04-15 22:04:08 +0200

    First commit- console prototype

---
+++ SOnB/MultiplierSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOnB
+{
+    class MultiplierSystem
+    {
+        public int MultiplyWithCheck(int firstNumber, int secondNumber, int modulo, int errorInMultiply = 0, int errorInCheck = 0)
+        {
+            int result = (firstNumber * secondNumber + errorInMultiply) & 15;
+            int moduloResult = result % modulo;
+
+            int checkResult = (firstNumber % modulo) * (secondNumber % modulo) + errorInCheck;
+            int checkModuloResult = checkResult % modulo;
+
+            if(moduloResult == checkModuloResult)
+            {
+                return result;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+    }
+}
---
+++ SOnB/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOnB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj podstawę kodu residualnego");
+            int codeNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            firstNumber = firstNumber & 15;
+            secondNumber = secondNumber & 15;
+
+            Console.WriteLine("{0} * {1}", firstNumber, secondNumber);
+
+            MultiplierSystem multiplierSystem1 = new MultiplierSystem();
+            MultiplierSystem multiplierSystem2 = new MultiplierSystem();
+
+            int result1 = multiplierSystem1.MultiplyWithCheck(firstNumber, secondNumber, codeNumber);
+            int result2= multiplierSystem2.MultiplyWithCheck(firstNumber, secondNumber, codeNumber);
+            
+            if (result1 != -1 && result2 == -1)
+            {
+                Console.WriteLine("Wynik to {0}, ale układ 2 nie jest sprawny", result1);
+
+            }
+            else if (result1 == -1 && result2 != -1)
+            {
+                Console.WriteLine("Wynik to {0}, ale układ 1 nie jest sprawny", result2);
+            }
+            else if(result1 == -1 && result2 == -1)
+            {
+                Console.WriteLine("Coś poszło nie tak, wynik1 = {0} , wynik2 = {1}", result1, result2);
+            }
+            else if (result1 == result2)
+            {
+                Console.WriteLine("Wynik to {0}, oba układy sprawne", result1);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

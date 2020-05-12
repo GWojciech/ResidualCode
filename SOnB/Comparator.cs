ab8e72cd98eca4debb92958bd2fcb74eaa92c0be
GWojciech <woitek@autograf.pl>
2020-05-12 21:15:42 +0200

    Added TCP/IP communication between threads

---
+++ SOnB/Comparator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SOnB
+{
+    class Comparator
+    {
+
+        private static string Serve()
+        {
+            TcpListener tcpLsn;
+            int i = 0;
+            Socket sckt;
+            string text ="";
+            Byte[] receivedBytes = new Byte[100];
+            tcpLsn = new TcpListener(IPAddress.Parse("127.0.0.1"), 2223); //zainicjiuj listenera na podanym porcie i adresie
+            tcpLsn.Start();
+            while (i < 2)
+            {
+                sckt = tcpLsn.AcceptSocket(); //funkcja blokujaca do czasu nadejscia - połaczenia
+                sckt.Receive(receivedBytes, receivedBytes.Length, 0);
+                text += System.Text.Encoding.ASCII.GetString(receivedBytes);
+                text += ";";
+                i++;
+
+            }
+            tcpLsn.Stop();
+            return text;
+        }
+        private void CompareResults(int result1, int result2)
+        {
+            Console.WriteLine("\nKomparator:");
+            if (result1 != -1 && result2 == -1)
+            {
+                Console.WriteLine("Wynik to {0}, ale układ 2 nie jest sprawny", result1);
+
+            }
+            else if (result1 == -1 && result2 != -1)
+            {
+                Console.WriteLine("Wynik to {0}, ale układ 1 nie jest sprawny", result2);
+            }
+            else if (result1 == -1 && result2 == -1)
+            {
+                Console.WriteLine("Coś poszło nie tak, wynik1 = {0} , wynik2 = {1}", result1, result2);
+            }
+            else if (result1 == result2)
+            {
+                Console.WriteLine("Wynik to {0}, oba układy sprawne", result1);
+            }
+        }
+
+        public void getData()
+        {
+            string tmp = Serve();
+            var values = tmp.Split(';');
+            CompareResults(Int32.Parse(values[0]), Int32.Parse(values[1]));
+
+        }
+    }
+}
--- SOnB/Program.cs
+++ SOnB/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SOnB
@@ -10,42 +12,44 @@ namespace SOnB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj pierwszą liczbę");
-            int firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj drugą liczbę");
-            int secondNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj podstawę kodu residualnego");
-            int codeNumber = Convert.ToInt32(Console.ReadLine());
-
-
-            firstNumber = firstNumber & 15;
-            secondNumber = secondNumber & 15;
-
-            Console.WriteLine("{0} * {1}", firstNumber, secondNumber);
-
             MultiplierSystem multiplierSystem1 = new MultiplierSystem();
             MultiplierSystem multiplierSystem2 = new MultiplierSystem();
+            EntranceElement entranceElement = new EntranceElement();
+            Comparator comparator = new Comparator();
+            Thread[] threads = new Thread[4];
+            string mode="";
 
-            int result1 = multiplierSystem1.MultiplyWithCheck(firstNumber, secondNumber, codeNumber);
-            int result2= multiplierSystem2.MultiplyWithCheck(firstNumber, secondNumber, codeNumber);
-            
-            if (result1 != -1 && result2 == -1)
+            Console.WriteLine("Podaj tryb: DEC, HEX, BIN");
+            while (true)
             {
-                Console.WriteLine("Wynik to {0}, ale układ 2 nie jest sprawny", result1);
+                mode = Console.ReadLine();
+                if(mode.Equals("DEC") || mode.Equals("HEX") || mode.Equals("BIN"))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Podaj mnożną");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Podaj mnożnik");
+            string secondNumber = Console.ReadLine();
+            Console.WriteLine("Podaj podstawę kodu");
+            string moduloNumber = Console.ReadLine();
 
-            }
-            else if (result1 == -1 && result2 != -1)
-            {
-                Console.WriteLine("Wynik to {0}, ale układ 1 nie jest sprawny", result2);
-            }
-            else if(result1 == -1 && result2 == -1)
-            {
-                Console.WriteLine("Coś poszło nie tak, wynik1 = {0} , wynik2 = {1}", result1, result2);
-            }
-            else if (result1 == result2)
-            {
-                Console.WriteLine("Wynik to {0}, oba układy sprawne", result1);
-            }
+            threads[0] = new Thread(delegate () { entranceElement.SendData(mode, firstNumber, secondNumber, moduloNumber); });
+            threads[0].Start();
+            threads[1] = new Thread(delegate () { comparator.getData(); });
+            threads[1].Start();
+
+            threads[2] = new Thread(delegate () { multiplierSystem1.getData(1); });
+            threads[2].Start();
+            threads[2].Join();
+
+            threads[3] = new Thread(delegate () { multiplierSystem2.getData(2); });
+            threads[3].Start();
+            threads[3].Join();
+
+            threads[0].Join();
+            threads[1].Join();
 
             Console.ReadKey();
 
